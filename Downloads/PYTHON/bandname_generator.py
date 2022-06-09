bd072fd4b70503b5eb3fb1560bc988e3870e9a92
Orienta <fabulousmyke2008@yahoo.com>
2022-06-09 01:38:36 +0100

    Bandname script

---
+++ Downloads/PYTHON/bandname_generator.py
@@ -0,0 +1,27 @@
+# String Manipulation using print function
+
+# print("Hello World\nHello World ")
+# print('string concatenation is done with  +  sign.')
+# print ("eg " + " Hello " + "" + " world")
+# print("New line can be created with a\n and n." )
+
+# # Input function
+# print("Hello " + input("Whats your name:") + "?")
+
+# Used Variable
+# # Index = input("Whats your name?:")
+# # print(len(Index))
+
+# # One Liner
+# print(len(input("Enter your name?")))
+
+# # emphasis on naming variables
+
+print("Welcome to band name generator!🙌")
+
+city = input("What city did you grow up in:\n")
+
+pet = input("Whats the name of your pet:\n")
+
+print("Your band name is" + city + " " + pet)
+
